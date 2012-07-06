043559818dc01817f02f2d8e73456631a9dc3a89
Fabio M. Costa <fabiomcosta@gmail.com>
2012-07-05 22:54:17 -0400

    just improving a little the code

--- Specs/helpers/server.js
+++ Specs/helpers/server.js
@@ -5,36 +5,37 @@ app.use(express.bodyParser());
 app.use(express['static'](__dirname + '../../../'));
 
 
-app.all('/', function(req, res) {
+var data = [
+    {id: 0, text: 'ajax'},
+    {id: 1, text: 'sim ajax'},
+    {id: 2, text: 'a very long text so we can test the width property properly'},
+    {id: 3, text: 'aaaaa'},
+    {id: 4, text: 'aff'},
+    {id: 5, text: 'alguma coisa'},
+    {id: 6, text: 'something'},
+    {id: 7, text: 'omg'},
+    {id: 8, text: 'testing'},
+    {id: 9, text: 'so what?'},
+    {id: 10, text: 'other chars'},
+    {id: 11, text: 'á latin é'},
+    {id: 12, text: 'hmmm'},
+    {id: 13, text: 'chars test > gg'},
+    {id: 14, text: 'chars test < gg'},
+    {id: 15, text: 'chars test & gg'},
+    {id: 16, text: 'ok'},
+    {id: 17, text: 'it\' fine'}
+];
 
-    var data = [
-        {id: 0, text: 'ajax'},
-        {id: 1, text: 'sim ajax'},
-        {id: 2, text: 'a very long text so we can test the width property properly'},
-        {id: 3, text: 'aaaaa'},
-        {id: 4, text: 'aff'},
-        {id: 5, text: 'alguma coisa'},
-        {id: 6, text: 'something'},
-        {id: 7, text: 'omg'},
-        {id: 8, text: 'testing'},
-        {id: 9, text: 'so what?'},
-        {id: 10, text: 'other chars'},
-        {id: 11, text: 'á latin é'},
-        {id: 12, text: 'hmmm'},
-        {id: 13, text: 'chars test > gg'},
-        {id: 14, text: 'chars test < gg'},
-        {id: 15, text: 'chars test & gg'},
-        {id: 16, text: 'ok'},
-        {id: 17, text: 'it\' fine'}
-    ];
+app.all('/', function(req, res) {
 
     res.header('Content-Type', 'application/json');
 
-    var items, id, q;
+    var items,
+        id = req.query.id,
+        q = req.query.q;
 
-    if (req.query.id !== undefined) {
+    if (id !== undefined) {
 
-        id = req.query.id;
         items = data.filter(function(item) {
             return item.id === id;
         });
@@ -42,9 +43,8 @@ app.all('/', function(req, res) {
             res.send(items);
         }
 
-    } else if (req.query.q !== undefined) {
+    } else if (q !== undefined) {
 
-        q = req.query.q;
         if (q) {
             items = data.filter(function(item) {
                 return item.text.indexOf(q) !== -1;
@@ -60,6 +60,7 @@ app.all('/', function(req, res) {
         res.send(items);
 
     }
+
 });
 
 app.listen(3000);
